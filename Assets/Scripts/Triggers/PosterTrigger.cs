35fb51d432cdc46b6e9e88274d5e6415f20e7ad8
Bolicrad <815763627@qq.com>
2023-03-12 09:48:36 -0600

    PosterTrigger Bugfix

--- Assets/Scripts/Triggers/PosterTrigger.cs
+++ Assets/Scripts/Triggers/PosterTrigger.cs
@@ -6,6 +6,7 @@ namespace Triggers
 {
     public class PosterTrigger : InteractTrigger
     {
+        [SerializeField] private ScrollRect scrollView;
         [SerializeField] private Image container;
         [SerializeField] private Sprite image;
 
@@ -13,12 +14,16 @@ namespace Triggers
         {
             base.InteractOnPerformed(obj);
             container.sprite = container.sprite == image ? null : image;
+            scrollView.gameObject.SetActive(container.sprite == image);
         }
 
         protected override void OnPlayerExit()
         {
             base.OnPlayerExit();
             container.sprite = null;
+            scrollView.gameObject.SetActive(false);
+            Canvas.ForceUpdateCanvases();
+            scrollView.verticalNormalizedPosition = 1f;
         }
     }
 }
